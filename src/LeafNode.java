94480151d06952067f101e1b6e3fd7c7036256cf
Ananiya <Ananiya@LAPTOP-94V63P7C>
2020-03-13 23:27:53 -0400

    Project Skeleton

---
+++ src/LeafNode.java
@@ -0,0 +1,19 @@
+/**
+ * Virginia Tech Honor Code Pledge:
+ *
+ * As a Hokie, I will conduct myself with honor
+ * and integrity at all times.
+ * I will not lie, cheat, or steal, nor will I
+ * accept the actions of those who do.
+ * -- Ananiya Admassu (ananiya0)
+ */
+
+/**
+ * Add the class description here.
+ *
+ * @author Ananiya Admassu (ananiya0)
+ * @version 03/13/2020
+ */
+public class LeafNode extends DNATreeNode {
+
+}
